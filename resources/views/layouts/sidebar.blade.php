f7661393f886f929a9d44366dcc439e5344869e7
eri-maulana <maulanabouble11@gmail.com>
2024-01-07 17:20:50 +0700

    memperbaiki tampilan dan layout

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,10 +1,10 @@
  <!-- Main Sidebar Container -->
  <aside class="main-sidebar elevation-4 sidebar-light-navy">
      <!-- Brand Logo -->
-     <a href="index3.html" class="brand-link bg-light ">
-         <img src="{{ asset('AdminLTE/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+     <a href="/" class="brand-link bg-light ">
+         <img src="{{ asset('img/sdm.jpg') }}" alt="Logo"
              class="brand-image img-circle elevation-3" style="opacity: .8">
-         <span class="brand-text font-weight-light">AdminLTE 3</span>
+         <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
      </a>
 
      <!-- Sidebar -->
@@ -12,11 +12,11 @@
          <!-- Sidebar user panel (optional) -->
          <div class="user-panel mt-3 pb-3 mb-3 d-flex">
              <div class="image">
-                 <img src="{{ asset('AdminLTE/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
+                 <img src="{{ asset('img/sdm.jpg') }}" class="img-circle elevation-2"
                      alt="User Image">
              </div>
              <div class="info">
-                 <a href="#" class="d-block">Alexander Pierce</a>
+                 <a href="#" class="d-block">{{ auth()->user()->name }}</a>
              </div>
          </div>
 
@@ -29,7 +29,7 @@
                  <!-- Add icons to the links using the .nav-icon class
               with font-awesome or any other icon font library -->
                  <li class="nav-item">
-                     <a href="#" class="nav-link active">
+                     <a href="{{ route('dashboard') }}" class="nav-link active">
                          <i class="fas fa-tachometer-alt mr-2"></i>
                          <p>
                              Dashboard
